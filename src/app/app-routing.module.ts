c475146918be8e66f2371d3a64a29f768a94e851
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-31 17:26:05 +0300

    add landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component'
 import { RepoComponent } from './repo/repo.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'user', component: UserComponent},
   { path: 'repo', component: RepoComponent},
+  { path: 'home', component: HomeComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
 import { RepoComponent } from './repo/repo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     UserComponent,
-    RepoComponent
+    RepoComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

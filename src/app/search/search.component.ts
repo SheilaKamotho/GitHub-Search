56d27797e2fd3001468f82741b076650e07927d1
Kamotho Sheila <kamothosheila@gmail.com>
2020-08-31 16:46:37 +0300

    delete http request details to start afresh

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  
 })
 export class SearchComponent implements OnInit {
-  userSearch(term:string) {
+ 
+  getUsers(term:string) {
   }
-  repoSearch(term:string) {
+  getRepos(term:string) {
   }
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,3 @@
 export class User {
-    constructor(public login:string, public name:string, public email:string, public public_repos:number, public followers:number, public following: number, public created_at: number ){}
+   
 }

070fa3846f607f3ba547dec07ba7bacd3013991c
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-01 11:24:51 +0300

    import user service to user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user-service/user.service';
+import { UserService } from '../user-service/user.service'
 
 @Component({
   selector: 'app-user',
@@ -7,9 +7,33 @@ import { UserService } from '../user-service/user.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  
-  
-    ngOnInit() {
-    }
-  
+  profile: any;
+  repos: any;
+  userName: any = 'SheilaKamotho'
+
+  constructor(private userService:UserService) {
+
   }
+
+  findProfile(){
+   this.userService.updateProfile(this.userName);
+
+   this.userService.getProfileInfo().subscribe(profile =>{
+     console.log(profile);
+   this.profile= profile      
+   });
+
+   this.userService.getProfileRepos().subscribe(repos =>{
+     console.log(repos);
+   this.repos= repos;      
+   
+   })
+ }
+
+ ngOnInit(){
+   this.findProfile()
+ }
+
+}
+  
+   
